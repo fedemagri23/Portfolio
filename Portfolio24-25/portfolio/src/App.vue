1dadeed60a500df07a7dd05cc276efaa171ce1d0
fedemagri <fmagri@itba.edu.ar>
2024-10-16 09:39:53 -0300

    modularized home page

--- Portfolio24-25/portfolio/src/App.vue
+++ Portfolio24-25/portfolio/src/App.vue
@@ -1,27 +1,15 @@
 <template>
   <v-app>
     <v-main>
-      <v-container class="bg-red h-screen w-100 d-flex justify-center align-center">
-        <v-container class="bg-green d-flex justify-center align-center h-50 w-66 rounded-lg">
-
-        </v-container>
-      </v-container>
-
-      <v-container class="bg-blue h-screen w-100">
-
-      </v-container>
+      <HomeScreen/>
     </v-main>
 
   </v-app>
 </template>
 
 <script setup>
+import HomeScreen from './components/screens/HomeScreen.vue';
+
   //
 </script>
-
-<style>
-  .background-image-container {
-    background-image: url('https://via.placeholder.com/1500');
-  }
-</style>
 
---
+++ Portfolio24-25/portfolio/src/components/screens/HomeScreen.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container class="background-image-container bg-red h-screen w-100 d-flex justify-center align-center">
+        <v-container class="bg-green d-flex justify-center align-center h-50 w-66 rounded-lg">
+
+        </v-container>
+      </v-container>
+
+      <v-container class="bg-blue h-screen w-100">
+
+      </v-container>
+</template>
+
+<style>
+  .background-image-container {
+    background-image: url('https://via.placeholder.com/1500');
+  }
+</style>
+
+<script setup>
+
+</script>
