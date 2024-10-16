5e5a9812df8e452cbfe4e7909017666b1a3bd736
fedemagri <fmagri@itba.edu.ar>
2024-10-16 09:31:57 -0300

    First commit
    Basic HmePage structure

---
+++ Portfolio24-25/portfolio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="bg-red h-screen w-100 d-flex justify-center align-center">
+        <v-container class="bg-green d-flex justify-center align-center h-50 w-66 rounded-lg">
+
+        </v-container>
+      </v-container>
+
+      <v-container class="bg-blue h-screen w-100">
+
+      </v-container>
+    </v-main>
+
+  </v-app>
+</template>
+
+<script setup>
+  //
+</script>
+
+<style>
+  .background-image-container {
+    background-image: url('https://via.placeholder.com/1500');
+  }
+</style>
+
