dbc4d93e2920225cf3f89dc219334529667ad5bb
fedemagri <fmagri@itba.edu.ar>
2024-10-16 10:03:13 -0300

    Started home page styling
    BG image setted, info container stylized and buttons created.

--- Portfolio24-25/portfolio/src/components/screens/HomeScreen.vue
+++ Portfolio24-25/portfolio/src/components/screens/HomeScreen.vue
@@ -1,7 +1,8 @@
 <template>
-    <v-container class="background-image-container bg-red h-screen w-100 d-flex justify-center align-center">
-        <v-container class="bg-green d-flex justify-center align-center h-50 w-66 rounded-lg">
-
+    <v-container class="background-image-container h-screen w-100 d-flex justify-center align-center">
+        <v-container class="opacity-90 bg-white d-flex justify-center align-center h-50 w-66 rounded-lg elevation-4">
+            <v-btn class="w-25 rounded-xl"/>
+            <v-btn class="w-25 rounded-xl"/>
         </v-container>
       </v-container>
 
@@ -10,10 +11,12 @@
       </v-container>
 </template>
 
-<style>
-  .background-image-container {
-    background-image: url('https://via.placeholder.com/1500');
-  }
+<style scoped>
+    .background-image-container {
+        background-image: url('https://firebasestorage.googleapis.com/v0/b/portfolio-127d7.appspot.com/o/coding-bg.jfif?alt=media&token=a1932cec-7a3b-499c-91e0-a322333defc6');
+        background-size: cover;
+        background-position: center;
+    }
 </style>
 
 <script setup>
