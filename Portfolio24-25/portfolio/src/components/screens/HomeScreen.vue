c991ee8982d40895b531e491199f005ac78dc376
fedemagri <fmagri@itba.edu.ar>
2024-10-17 21:52:57 -0300

    Added skills internal navigation.

--- Portfolio24-25/portfolio/src/components/screens/HomeScreen.vue
+++ Portfolio24-25/portfolio/src/components/screens/HomeScreen.vue
@@ -58,110 +58,155 @@
 
     <Section ref="about" class="bg-background w-100 ma-0 px-6" style="min-height: 100vh;">
         <v-col style="padding-top: 106px;">
-            <h1>About me</h1>
+            <h1 style="font-size: 3rem;">About me</h1>
             
         </v-col>
     </Section>
 
     <Section ref="education" class="bg-background w-100 ma-0 px-6" style="min-height: 100vh;">
         <v-col style="padding-top: 106px;">
-            <h1>Education</h1>
+            <h1 style="font-size: 3rem;">Education</h1>
         </v-col>
     </Section>
 
     <Section ref="skills" class="bg-background h-50 w-100 ma-0 px-6" style="min-height: 100vh;">
         <v-col style="padding-top: 106px;">
-            <h1 class="mb-4">My skills</h1>
-            <h2>Programming languages</h2>
-            <v-row class="my-2">
 
-                <v-col xs="6" sm="4" md="4" lg="4" xl="2" class="rounded-ts-xl d-flex flex-column align-center border-lg pa-4">
-                    <v-container class="d-flex flex-row justify-center align-center w-100 h-25">
-                        <v-icon class="text-colortext1 ma-2" size="64">mdi-language-c</v-icon>
-                        <h1 class="text-colortext1 font-weight-bold">C & C++</h1>
+            <transition name="slide">
+                <div v-if="showSkillNav" class="d-flex flex-row align-center justify-center py-2 px-6 w-50" style="position: fixed; top: 10vh; right: 0; z-index: 9999;">
+                    <v-container @click="scrollToSection('languages')" class="nav-bar-item bg-primary rounded-lg mx-3 my-1 pa-2 elevation-4 cursor-pointer d-flex justify-center align-center">
+                        <h3>Programming languages</h3>
                     </v-container>
-                    <p class="font-weight-medium">
-                        One of my favorite languages, thanks to <b>C</b> I developed my knowledge and skills regarding the internal functioning 
-                        of the computer and how programs, compilers and linkeditors work.
-                    </p>
-                    <br>
-                    <p class="font-weight-medium">
-                        The contents that I handle without any problems are: Characteristics of the <b>imperative paradigm</b>, functions and 
-                        modularization, process <b>efficiency</b>, <b>recursion</b>, <b>library</b> use and construction, pointers, structs, <b>abstract data 
-                        types</b> and memory management.
-                    </p>
-                    <br>
-                    <p class="font-weight-medium">
-                        My specialty is processing <b>large files</b> to get the output in the shortest time possible using structures like <b>AVL 
-                        trees</b>, <b>hash tables</b>, <b>linked lists</b> and occurrence vectors, and algorithms such as <b>binary seacrh</b>, DFS, BFS, 
-                        dijkstra, <b>merge sort</b>, <b>quick sort</b>, among others.
-                    </p>
-                </v-col>
-
-                <v-col xs="6" sm="6" md="4" lg="4" xl="2" class="d-flex flex-column align-center border-lg pa-4">
-                    <v-container class="d-flex flex-row justify-center align-center w-100 h-25">
-                        <v-icon class="text-colortext1 ma-2" size="64">mdi-language-java</v-icon>
-                        <h1 class="text-colortext1 font-weight-bold">Java</h1>
+                    <v-container @click="scrollToSection('frameworks')" class="nav-bar-item bg-primary rounded-lg mx-3 my-1 pa-2 elevation-4 cursor-pointer d-flex justify-center align-center">
+                        <h3>Frameworks</h3>
                     </v-container>
-                    <p class="font-weight-medium">
-                        <b>Java</b> is the language that I use the most today due to the degree I am studying.
-                    </p>
-                    <br>
-                    <p class="font-weight-medium">
-                        I have excellent knowledge of the <b>object-oriented paradigm</b>, with complete knowledge of <b>classes</b>, 
-                        <b>inheritance</b>, <b>interfaces</b>, <b>generics</b>, abstract classes, encapsulation, hiding, polymorphism, error messages and methods.
-                    </p>
-                    <br>
-                    <p class="font-weight-medium">
-                        In addition, implementing strategies for different needs, such as <b>iterators</b>, predicates, lambda functions, equals and <b>hashing</b>.
-                    </p>
-                </v-col>
+                </div>
+            </transition>
 
-                <v-col xs="6" sm="6" md="4" lg="4" xl="2" class="d-flex flex-column align-center border-lg pa-4">
-                    <v-container class="d-flex flex-row justify-center align-center w-100 h-25">
-                        <v-icon class="text-colortext1 ma-2" size="64">mdi-language-ruby</v-icon>
-                        <h1 class="text-colortext1 font-weight-bold">Ruby</h1>
-                    </v-container>
-                </v-col>
+            <h1 style="font-size: 3rem;" class="mb-4">My skills</h1>
 
-                <v-col xs="6" sm="6" md="4" lg="4" xl="2" class="d-flex flex-column align-center border-lg pa-4">
-                    <v-container class="d-flex flex-row justify-center align-center w-100 h-25">
-                        <v-icon class="text-colortext1 ma-2" size="64">mdi-language-javascript</v-icon>
-                        <h1 class="text-colortext1 font-weight-bold">JavaScript</h1>
-                    </v-container>
-                </v-col>
+            <Section ref="languages" class="w-100 ma-0 px-0" style="padding-top: 106px">
+                <h2>Programming languages</h2>
+                <v-row class="my-2">
 
-                <v-col xs="6" sm="6" md="4" lg="4" xl="2" class="d-flex flex-column align-center border-lg pa-4">
-                    <v-container class="d-flex flex-row justify-center align-center w-100 h-25">
-                        <v-icon class="text-colortext1 ma-2" size="64">mdi-bash</v-icon>
-                        <h1 class="text-colortext1 font-weight-bold">Assembler(Intel) & Bash</h1>
-                    </v-container>
-                </v-col>
+                    <v-col xs="6" sm="4" md="4" lg="4" xl="2" class="rounded-ts-xl d-flex flex-column align-center border-lg pa-4">
+                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
+                            <v-icon class="text-colortext1 ma-2" size="64">mdi-language-c</v-icon>
+                            <h1 class="text-colortext1 font-weight-bold">C & C++</h1>
+                        </v-container>
+                        <p class="font-weight-medium">
+                            One of my favorite languages, thanks to <b>C</b> I developed my knowledge and skills regarding the internal functioning 
+                            of the computer and how programs, compilers and linkeditors work.
+                        </p>
+                        <br>
+                        <p class="font-weight-medium">
+                            The contents that I handle without any problems are: Characteristics of the <b>imperative paradigm</b>, functions and 
+                            modularization, process <b>efficiency</b>, <b>recursion</b>, <b>library</b> use and construction, pointers, structs, <b>abstract data 
+                            types</b> and memory management.
+                        </p>
+                        <br>
+                        <p class="font-weight-medium">
+                            My specialty is processing <b>large files</b> to get the output in the shortest time possible using structures like <b>AVL 
+                            trees</b>, <b>hash tables</b>, <b>linked lists</b> and occurrence vectors, and algorithms such as <b>binary seacrh</b>, DFS, BFS, 
+                            dijkstra, <b>merge sort</b>, <b>quick sort</b>, among others.
+                        </p>
+                    </v-col>
 
-                <v-col xs="6" sm="4" md="4" lg="4" xl="2" class="rounded-be-xl d-flex flex-column align-center border-lg pa-4">
-                    <v-container class="d-flex flex-row justify-center align-center w-100 h-25">
-                        <v-icon class="text-colortext1 ma-2" size="64">mdi-language-python</v-icon>
-                        <h1 class="text-colortext1 font-weight-bold">Python</h1>
-                    </v-container>
-                </v-col>
+                    <v-col xs="6" sm="6" md="4" lg="4" xl="2" class="d-flex flex-column align-center border-lg pa-4">
+                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
+                            <v-icon class="text-colortext1 ma-2" size="64">mdi-language-java</v-icon>
+                            <h1 class="text-colortext1 font-weight-bold">Java</h1>
+                        </v-container>
+                        <p class="font-weight-medium">
+                            <b>Java</b> is the language that I use the most today due to the degree I am studying.
+                        </p>
+                        <br>
+                        <p class="font-weight-medium">
+                            I have excellent knowledge of the <b>object-oriented paradigm</b>, with complete knowledge of <b>classes</b>, 
+                            <b>inheritance</b>, <b>interfaces</b>, <b>generics</b>, abstract classes, encapsulation, hiding, polymorphism, error messages and methods.
+                        </p>
+                        <br>
+                        <p class="font-weight-medium">
+                            In addition, implementing strategies for different needs, such as <b>iterators</b>, predicates, lambda functions, equals and <b>hashing</b>.
+                        </p>
+                        <br>
+                        <p class="font-weight-medium">
+                            Taking advantage of all of the above, I usually use the algorithmic strategies of <b>divide & conquer</b>, backtracking, greedy and dynamic programming 
+                            to structure the different data structures that the language offers.
+                        </p>
+                        <br>
+                        <p class="font-weight-medium">
+                            Lastly, I use the following libraries: <b>Metaphone</b> (fuzzy text search) and <b>Lucene</b> (index for text search requests).
+                        </p>
+                    </v-col>
 
-            </v-row>
+                    <v-col xs="6" sm="6" md="4" lg="4" xl="2" class="d-flex flex-column align-center border-lg pa-4">
+                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
+                            <v-icon class="text-colortext1 ma-2" size="64">mdi-language-ruby</v-icon>
+                            <h1 class="text-colortext1 font-weight-bold">Ruby</h1>
+                        </v-container>
+                    </v-col>
 
-            <h2>Frameworks</h2>
-            <v-row class="my-2">
-                <v-col cols="6" class="bg-red rounded-s-xl">
-                    Vue
-                </v-col>
-                <v-col cols="6" class="bg-yellow">
-                    React native
-                </v-col>
-            </v-row>
+                    <v-col xs="6" sm="6" md="4" lg="4" xl="2" class="d-flex flex-column align-center border-lg pa-4">
+                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
+                            <v-icon class="text-colortext1 ma-2" size="64">mdi-language-javascript</v-icon>
+                            <h1 class="text-colortext1 font-weight-bold">JavaScript</h1>
+                        </v-container>
+                    </v-col>
+
+                    <v-col xs="6" sm="6" md="4" lg="4" xl="2" class="d-flex flex-column align-center border-lg pa-4">
+                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
+                            <v-icon class="text-colortext1 ma-2" size="64">mdi-bash</v-icon>
+                            <h1 class="text-colortext1 font-weight-bold">Assembler(Intel) & Bash</h1>
+                        </v-container>
+                    </v-col>
+
+                    <v-col xs="6" sm="4" md="4" lg="4" xl="2" class="rounded-be-xl d-flex flex-column align-center border-lg pa-4">
+                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
+                            <v-icon class="text-colortext1 ma-2" size="64">mdi-language-python</v-icon>
+                            <h1 class="text-colortext1 font-weight-bold">Python</h1>
+                        </v-container>
+                    </v-col>
+
+                </v-row>
+            </Section>
+            
+            <Section ref="frameworks" class="w-100 ma-0 px-0" style="padding-top: 106px">
+                <h2>Frameworks</h2>
+                <v-row class="my-2">
+
+                    <v-col cols="6" class="rounded-ts-xl d-flex flex-column align-center border-lg pa-4">
+                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
+                            <v-icon class="text-colortext1 ma-2" size="64">mdi-vuejs</v-icon>
+                            <h1 class="text-colortext1 font-weight-bold">Vue</h1>
+                        </v-container>
+                        <p class="font-weight-medium">
+
+                        </p>
+                        <br>
+
+                    </v-col>
+
+                    <v-col cols="6" class="rounded-be-xl d-flex flex-column align-center border-lg pa-4">
+                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
+                            <v-icon class="text-colortext1 ma-2" size="64">mdi-react</v-icon>
+                            <h1 class="text-colortext1 font-weight-bold">React Native</h1>
+                        </v-container>
+                        <p class="font-weight-medium">
+
+                        </p>
+                        <br>
+                    </v-col>
+
+                </v-row>
+            </Section>
+            
         </v-col>
     </Section>
 
     <Section ref="projects" class="bg-background h-25 w-100  ma-0 px-6" style="min-height: 100vh;">
         <v-col style="padding-top: 106px;">
-            <h1>Projects</h1>
+            <h1 style="font-size: 3rem;">Projects</h1>
         </v-col>
     </Section>
 
@@ -179,11 +224,21 @@
     .nav-bar.sticky {
         backdrop-filter: blur(30px);
     }
-    .slide-enter-active, .slide-leave-active {
+    
+    .slide-enter-active{
+        transition: all 0.5s ease;
+        opacity: 0;
+    }
+    
+    .slide-enter-to {
+        opacity: 1;
+    }
+
+    .slide-leave-active {
         transition: all 0.5s ease;
     }
-    .slide-enter, .slide-leave-to {
-        transform: translateX(-40px);
+    .slide-leave-to {
+        transform: translateY(-40px);
         opacity: 0;
     }
     .nav-bar-item :hover {
@@ -218,6 +273,8 @@
     const skills = ref(null);
     const banner = ref(null);
     const projects = ref(null);
+    const languages = ref(null);
+    const frameworks = ref(null);
 
     const scrollToSection = (section) => {
         if (section === 'about' && about.value) {
@@ -230,10 +287,15 @@
             banner.value.scrollIntoView({ behavior: 'smooth' });
         } else if (section === 'projects' && projects.value) {
             projects.value.scrollIntoView({ behavior: 'smooth' });
+        } else if (section === 'languages' && languages.value) {
+            languages.value.scrollIntoView({ behavior: 'smooth' });
+        } else if (section === 'frameworks' && frameworks.value) {
+            frameworks.value.scrollIntoView({ behavior: 'smooth' });
         }
     };
 
     var showBanner = ref(false);
+    var showSkillNav = ref(false);
 
     window.addEventListener("scroll", function(){
         var header = document.querySelector(".nav-bar");
@@ -249,7 +311,13 @@
             header.classList.add("py-6");
             item.classList.add("bg-primary");
             showBanner.value = false;
-    }
+        }
+
+        if (window.scrollY >= 2.85*window.innerHeight){
+            showSkillNav.value = true;
+        } else {
+            showSkillNav.value = false;
+        }
     });
 
 </script>
