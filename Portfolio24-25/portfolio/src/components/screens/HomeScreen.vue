3d9fa8a592b48e859f8a096c4812d65113d255f9
fedemagri <fmagri@itba.edu.ar>
2024-10-18 22:45:25 -0300

    Modularizated skills sections in components.

--- Portfolio24-25/portfolio/src/components/screens/HomeScreen.vue
+++ Portfolio24-25/portfolio/src/components/screens/HomeScreen.vue
@@ -68,174 +68,12 @@
 
             <Section ref="languages" class="w-100 ma-0 px-0" style="padding-top: 106px">
                 <h2>Programming languages</h2>
-                <v-row class="my-2">
-
-                    <v-col xs="6" sm="6" md="4" lg="4" xl="2" class="rounded-ts-xl d-flex flex-column align-center border-lg pa-4">
-                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
-                            <v-icon class="text-colortext1 ma-2" size="64">mdi-language-c</v-icon>
-                            <h1 class="text-colortext1 font-weight-bold">C & C++</h1>
-                        </v-container>
-                        <v-progress-linear color="green" model-value="90" :height="6"></v-progress-linear>
-                        <br>
-                        <p class="font-weight-medium">
-                            One of my favorite programming languages. Thanks to <b>C</b>, I developed my knowledge and skills regarding the internal workings of computers and how programs, compilers, and linkeditors work.
-                        </p>
-                        <br>
-                        <p class="font-weight-medium">
-                            The topics I handle proficiently include: characteristics of the <b>imperative paradigm</b>, functions and modularization, process <b>efficiency</b>, <b>recursion</b>, <b>library</b> usage and construction, pointers, structs, <b>abstract data types</b>, and memory management.
-                        </p>
-                        <br>
-                        <p class="font-weight-medium">
-                            My specialty lies in processing <b>large files</b> to achieve output in the shortest time possible, utilizing structures like <b>AVL trees</b>, <b>hash tables</b>, <b>linked lists</b>, and occurrence vectors, as well as algorithms such as <b>binary search</b>, DFS, BFS, Dijkstra, <b>merge sort</b>, <b>quick sort</b>, among others.
-                        </p>
-                    </v-col>
-
-
-                    <v-col xs="6" sm="6" md="4" lg="4" xl="2" class="d-flex flex-column align-center border-lg pa-4">
-                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
-                            <v-icon class="text-colortext1 ma-2" size="64">mdi-language-java</v-icon>
-                            <h1 class="text-colortext1 font-weight-bold">Java</h1>
-                        </v-container>
-                        <v-progress-linear color="green" model-value="80" :height="6"></v-progress-linear>
-                        <br>
-                        <p class="font-weight-medium">
-                            <b>Java</b> is the language I use the most today due to my studies.
-                        </p>
-                        <br>
-                        <p class="font-weight-medium">
-                            I have a strong understanding of the <b>object-oriented paradigm</b>, with comprehensive knowledge of <b>classes</b>, 
-                            <b>inheritance</b>, <b>interfaces</b>, <b>generics</b>, abstract classes, encapsulation, hiding, polymorphism, exception handling, and methods.
-                        </p>
-                        <br>
-                        <p class="font-weight-medium">
-                            Additionally, I implement strategies for various needs, such as <b>iterators</b>, predicates, lambda functions, equality checks, and <b>hashing</b>.
-                        </p>
-                        <br>
-                        <p class="font-weight-medium">
-                            Utilizing all of the above, I typically apply algorithmic strategies such as <b>divide and conquer</b>, backtracking, greedy algorithms, and dynamic programming 
-                            to structure the various data structures that the language offers.
-                        </p>
-                        <br>
-                        <p class="font-weight-medium">
-                            Finally, I work with the following libraries: <b>Metaphone</b> (fuzzy text search) and <b>Lucene</b> (indexing for text search queries).
-                        </p>
-
-                    </v-col>
-
-                    <v-col xs="6" sm="6" md="4" lg="4" xl="2" class="d-flex flex-column align-center border-lg pa-4">
-                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
-                            <v-icon class="text-colortext1 ma-2" size="64">mdi-language-ruby</v-icon>
-                            <h1 class="text-colortext1 font-weight-bold">Ruby</h1>
-                        </v-container>
-                        <v-progress-linear color="yellow" model-value="65" :height="6"></v-progress-linear>
-                        <br>
-                        <p class="font-weight-medium text-align-justify">
-                            I have a solid foundation in Ruby, which I explored extensively during my university studies. Its elegant syntax and focus on simplicity make it a 
-                            pleasure to work with.
-                        </p>
-                        <p class="font-weight-medium text-align-justify">
-                            I’m well-versed in Ruby's core features, including collections (arrays and hashes), the splat operator, and advanced techniques like metaprogramming and
-                            closures. While I haven't used Ruby in a professional context or with any frameworks, my understanding of its principles equips me with a <b>versatile skill</b>
-                            set for various programming challenges.
-                        </p>
-                    </v-col>
-
-                    <v-col xs="6" sm="6" md="4" lg="4" xl="2" class="d-flex flex-column align-center border-lg pa-4">
-                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
-                            <v-icon class="text-colortext1 ma-2" size="64">mdi-language-javascript</v-icon>
-                            <h1 class="text-colortext1 font-weight-bold">JavaScript</h1>
-                        </v-container>
-                        <v-progress-linear color="green" model-value="65" :height="6"></v-progress-linear>
-                        <br>
-                        <p class="font-weight-medium text-align-justify">
-                            JavaScript is my go-to language for making websites interactive and engaging. With its powerful features, I can create dynamic user interfaces that respond to 
-                            actions in real time. Whether it's animating elements, handling form submissions, or fetching data from <b>APIs</b>, JavaScript helps me bring ideas to life. 
-                            I also enjoy using modern JavaScript techniques, like <b>ES6</b> features and async/await, which make my code cleaner and more efficient. Plus, with <b>Node.js</b>, 
-                            I dive into server-side development, allowing me to build full-stack applications that seamlessly connect the front end with the back end. To get a clearer picture 
-                            of my skills, feel free to check out my projects below!
-                        </p>
-                    </v-col>
-
-                    <v-col xs="6" sm="6" md="4" lg="4" xl="2" class="d-flex flex-column align-center border-lg pa-4">
-                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
-                            <v-icon class="text-colortext1 ma-2" size="64">mdi-bash</v-icon>
-                            <h1 class="text-colortext1 font-weight-bold">Assembler(Intel) & Bash</h1>
-                        </v-container>
-                        <v-progress-linear color="blue" model-value="99" :height="6"></v-progress-linear>
-                        <br>
-                        <p class="font-weight-medium text-align-justify">
-                            Since my studies require complete in-depth knowledge of the computer, I adapted to the use of <b>Linux</b> and its functionalities, 
-                            which is why creating <b>bash scripts</b> is an everyday part of my daily life, to facilitate processes, configure program startups 
-                            or document management. Worked with bash for projects related to XML and HTML documents, C programming, Assembler and more.
-                        </p>
-                        <br>
-                        <p class="font-weight-medium text-align-justify">
-                            Continuing with the in-depth study of computers, I began to use <b>ASM Intel</b> to work with processors of that brand. The knowledge acquired 
-                            lies not only in basic instructions, but also in the correct handling of functions as standardized by <b>GCC</b>, complete knowledge of the use 
-                            of the stack and building of the stack frame, arguments, <b>syscalls</b>, <b>interruptions</b> and peripherals. All this in both <b>32 and 64 bits</b>, along 
-                            with their differences.
-                        </p>
-                        <br>
-                        <p class="font-weight-medium text-align-justify">
-                            I am currently working on the implementation of a <b>kernel</b> that manages the hardware resources of a computer and show features of <b>Intel 
-                            Protected Mode</b>.
-                        </p>
-                    </v-col>
-
-                    <v-col xs="6" sm="6" md="4" lg="4" xl="2" class="rounded-be-xl d-flex flex-column align-center border-lg pa-4">
-                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
-                            <v-icon class="text-colortext1 ma-2" size="64">mdi-language-python</v-icon>
-                            <h1 class="text-colortext1 font-weight-bold">Python</h1>
-                        </v-container>
-                        <v-progress-linear color="green" model-value="80" :height="6"></v-progress-linear>
-                        <br>
-                        <p class="font-weight-medium text-align-justify">
-                            Python is my preferred language for data analysis, automation, and web development. Its clear syntax and readability make it easy to write and maintain 
-                            code, allowing me to focus on solving problems rather than getting bogged down in complexity. 
-                        </p>
-                        <br>
-                        <p class="font-weight-medium text-align-justify">
-                            I leverage Python's powerful libraries, like Pandas and 
-                            NumPy, for data manipulation and analysis, as well as Flask and Django for building robust web applications. Whether I'm <b>scraping data</b> from the 
-                            web or creating <b>RESTful APIs</b>, Python provides the versatility I need to tackle diverse projects.
-                        </p>
-                    </v-col>
-
-                </v-row>
+                <ProgrammingLgSection/>
             </Section>
             
             <Section ref="frameworks" class="w-100 ma-0 px-0" style="padding-top: 106px">
                 <h2>Frameworks</h2>
-                <v-row class="my-2">
-
-                    <v-col cols="6" class="rounded-ts-xl d-flex flex-column align-center border-lg pa-4">
-                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
-                            <v-icon class="text-colortext1 ma-2" size="64">mdi-vuejs</v-icon>
-                            <h1 class="text-colortext1 font-weight-bold">Vue</h1>
-                        </v-container>
-                        <v-progress-linear color="primary" indeterminate="" :height="6"></v-progress-linear>
-                        <br>
-                        <p class="font-weight-medium">
-
-                        </p>
-                        <br>
-
-                    </v-col>
-
-                    <v-col cols="6" class="rounded-be-xl d-flex flex-column align-center border-lg pa-4">
-                        <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
-                            <v-icon class="text-colortext1 ma-2" size="64">mdi-react</v-icon>
-                            <h1 class="text-colortext1 font-weight-bold">React Native</h1>
-                        </v-container>
-                        <v-progress-linear color="yellow" model-value="50" :height="6"></v-progress-linear>
-                        <br>
-                        <p class="font-weight-medium">
-
-                        </p>
-                        <br>
-                    </v-col>
-
-                </v-row>
+                <FrameworksSection/>
             </Section>
             
         </v-col>
@@ -298,6 +136,8 @@
     import { ref } from 'vue';
     import Header from '../Header.vue';
     import EducationSection from '../sections/EducationSection.vue';
+import ProgrammingLgSection from '../sections/ProgrammingLgSection.vue';
+import FrameworksSection from '../sections/FrameworksSection.vue';
 
     const about = ref(null);
     const education = ref(null);
---
+++ Portfolio24-25/portfolio/src/components/sections/FrameworksSection.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-row class="my-2">
+
+        <v-col cols="6" class="rounded-ts-xl d-flex flex-column align-center border-lg pa-4">
+            <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
+                <v-icon class="text-colortext1 ma-2" size="64">mdi-vuejs</v-icon>
+                <h1 class="text-colortext1 font-weight-bold">Vue</h1>
+            </v-container>
+            <v-progress-linear color="primary" indeterminate :height="6"></v-progress-linear>
+            <br>
+            <p class="font-weight-medium">
+
+            </p>
+            <br>
+
+        </v-col>
+
+        <v-col cols="6" class="rounded-be-xl d-flex flex-column align-center border-lg pa-4">
+            <v-container class="d-flex flex-row justify-center align-center w-100" style="height: 128px;">
+                <v-icon class="text-colortext1 ma-2" size="64">mdi-react</v-icon>
+                <h1 class="text-colortext1 font-weight-bold">React Native</h1>
+            </v-container>
+            <v-progress-linear color="yellow" model-value="50" :height="6"></v-progress-linear>
+            <br>
+            <p class="font-weight-medium">
+
+            </p>
+            <br>
+        </v-col>
+
+    </v-row>
+</template>
